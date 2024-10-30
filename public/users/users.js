a17bd14dcbba0b87f532b658901236622e60642e
Misaint20 <zmisaintm@gmail.com>
2024-10-30 16:11:37 -0700

    Inicio de proyecto

---
+++ public/users/users.js
@@ -0,0 +1,14 @@
+// Fetch para obtener los usuarios desde la API
+fetch('/api/users')
+.then(response => response.json())
+.then(users => {
+  const userList = document.getElementById('user-list');
+  users.forEach(user => {
+    const li = document.createElement('li');
+    li.innerHTML = `<strong>Usuario:</strong> ${user.name} - <strong>Email:</strong> ${user.email}`;
+    userList.appendChild(li);
+  });
+})
+.catch(error => {
+  console.error('Error al obtener usuarios:', error);
+});
